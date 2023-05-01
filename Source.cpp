4cb59738c366b6ff42d3d3394f3e88fce2bac63f
Stanislav Bezzhanov <sbezzhanov@yandex.ru>
2023-05-01 15:08:08 +0300

    changed class constructors

--- Source.cpp
+++ Source.cpp
@@ -4,67 +4,39 @@
 class Figure {
 
 public:
-	Figure() {
-		Figure::Figure(sides_count, name);
-	};
-	int get_sides_count() { return this->sides_count; };
-
-	std::string get_name() { return this->name; };
-
-protected:
-	
-	int sides_count = 0;
-	std::string name = "Фигура";
-	
+	Figure() {};
 	Figure(int sides_count, std::string name) {
 		sides_count = this->sides_count;
 		name = this->name;
 	};
+	int get_sides_count() { return this->sides_count = sides_count; };
 
+	std::string get_name() { return this->name = name; };
+
+protected:
 	
+	int sides_count = 0;
+	std::string name = "Фигура";	
 };
 
 class Triangle : public Figure {
 
 public:
 	Triangle() {};
-	
-	int get_sides_count() {return this->sides_count; };
-
-	std::string get_name() {return this->name;	};
-
-protected:
-		int sides_count = 3;
-		std::string name = "Треугольник";
-
 	Triangle(int sides_count, std::string name) : Figure(sides_count, name) {
-		sides_count = this->sides_count;
-		name = this->name;
-	};
-
+		this->sides_count = sides_count;
+		this->name = name;
+	};	
 };
 
 class Quadrangle : public Figure {
 
 public:
 	Quadrangle() {};
-		
-	int get_sides_count() {
-		return this->sides_count;
-	};
-	std::string get_name() {
-		return this->name;
-	}
-		
-protected:
-
-	int sides_count = 4;
-	std::string name = "Четырехугольник";
 	Quadrangle(int sides_count, std::string name) : Figure(sides_count, name) {
-		sides_count = this->sides_count;
-		name = this->name;
+		this->sides_count = sides_count;
+		this->name = name;
 	};
-
 };
 
 int main() {
@@ -73,10 +45,10 @@ int main() {
 	std::cout << "Количество сторон: " << std::endl;
 	std::cout << f.get_name() << " : " << f.get_sides_count() << std::endl;
 	
-	Triangle t;
+	Triangle t(3, "Треугольник");
 	std::cout << t.get_name() << " : " << t.get_sides_count() << std::endl;
 	
-	Quadrangle q;
+	Quadrangle q(4, "Четырехугольник");
 	std::cout << q.get_name() << " : " << q.get_sides_count() << std::endl;
 	
 	return 0;
