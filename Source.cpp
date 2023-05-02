517f0785c61be172ea62ec32a8a2e70332d0612f
Stanislav Bezzhanov <sbezzhanov@yandex.ru>
2023-05-02 22:03:29 +0300

    all fixed

--- Source.cpp
+++ Source.cpp
@@ -1,55 +1,56 @@
-﻿#include <iostream>
-
+﻿#include <iostream> 
 
 class Figure {
 
 public:
-	Figure() {};
-	Figure(int sides_count, std::string name) {
-		sides_count = this->sides_count;
-		name = this->name;
+	Figure() {
+		this->sides_count = 0;
+		this->name = "Фигура";
 	};
-	int get_sides_count() { return this->sides_count = sides_count; };
-
-	std::string get_name() { return this->name = name; };
+	int get_sides_count() { return this->sides_count; };
+	std::string get_name() { return this->name; };
 
 protected:
-	
-	int sides_count = 0;
-	std::string name = "Фигура";	
+	int sides_count{};
+	std::string name{};
+
 };
 
 class Triangle : public Figure {
 
 public:
-	Triangle() {};
-	Triangle(int sides_count, std::string name) : Figure(sides_count, name) {
-		this->sides_count = sides_count;
-		this->name = name;
-	};	
+	Triangle() {
+		sides_count = 3;
+		name = "Треугольник";
+	};
+
+protected:
+	
+
 };
 
 class Quadrangle : public Figure {
 
 public:
-	Quadrangle() {};
-	Quadrangle(int sides_count, std::string name) : Figure(sides_count, name) {
-		this->sides_count = sides_count;
-		this->name = name;
+	Quadrangle() {
+		sides_count = 4;
+		name = "Четырехугольник";
 	};
+
+protected:
+
+
 };
 
 int main() {
 	setlocale(LC_ALL, "Ru");
 	Figure f;
 	std::cout << "Количество сторон: " << std::endl;
-	std::cout << f.get_name() << " : " << f.get_sides_count() << std::endl;
-	
-	Triangle t(3, "Треугольник");
-	std::cout << t.get_name() << " : " << t.get_sides_count() << std::endl;
-	
-	Quadrangle q(4, "Четырехугольник");
-	std::cout << q.get_name() << " : " << q.get_sides_count() << std::endl;
-	
+	std::cout << f.get_name() << ' ' << f.get_sides_count() << std::endl;
+	Triangle t;
+	std::cout << t.get_name() << ' ' << t.get_sides_count() << std::endl;
+	Quadrangle q;
+	std::cout << q.get_name() << ' ' << q.get_sides_count() << std::endl;
+
 	return 0;
 }
